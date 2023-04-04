f3d9d64509a9ade8b99c93059eeb78589e7bdc5b
Minsung Kim <dytnfrlaalst@naver.com>
2023-04-04 17:29:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,9 +1,9 @@
 def solution(s):
     answer = ''
-
+    a = len(s)//2
     if len(s) % 2 == 0:
-        answer = s[(len(s)//2)-1:(len(s)//2)+1]
+        answer = s[a-1:a+1]
     else: 
-        answer = s[(len(s)//2):(len(s)//2)+1]
+        answer = s[a:a+1]
     
     return answer
