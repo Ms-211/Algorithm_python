dd29726d468531be2e2813d158dc6cc6eaae190e
Minsung Kim <dytnfrlaalst@naver.com>
2023-04-07 14:22:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,5 +1,4 @@
 def solution(n):
-    answer = 0
     result = ''
 
     while n > 0:
@@ -7,6 +6,3 @@ def solution(n):
         result += str(mod)
              
     return int(result,3)
-
-
-# n을 3으로 계속 나눠
